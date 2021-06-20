2c15a126332138fe6c465273faacc8ca9da6b4d1
Nandu-R-Nair <67908205+Nandu-R-Nair@users.noreply.github.com>
2021-06-20 13:16:56 +0530

    Add files via upload

--- myfile.py
+++ myfile.py
@@ -1,74 +1,48 @@
-import audioread
-import sounddevice as sd
-import struct
-import wave
-import contextlib
-import sys
-import glob
-from scipy.io import wavfile as wav
-import matplotlib.pyplot as plt
-import IPython.display as ipd
 import numpy as np
-import pandas as pd
-import librosa
-import wave
-from scipy.io import wavfile as wav
-from sklearn.externals import joblib
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.decomposition import PCA
-out='D:/mainproject/babycrydata/output/'
-inpath=input("PATH  :   ")
+from flask import Flask, flash, request, jsonify, render_template
+from werkzeug.utils import secure_filename
+import pickle
+import os
 
-ext = (inpath.split('/')[-1]).split('.')[-1]
-#------------------------convert start -------------------------------------
-filename = inpath
-if(ext=='3gp'):        
-    with audioread.audio_open(filename) as f:
-#         print('Input file: %i channels at %i Hz; %.1f seconds.' %
-#               (f.channels, f.samplerate, f.duration),
-#               file=sys.stderr)
-#         print('Backend:', str(type(f).__module__).split('.')[1],
-#               file=sys.stderr)
-        filename_new = out + filename.split('/')[-1]
-        with contextlib.closing(wave.open(filename_new + '.wav', 'w')) as of:
-            of.setnchannels(f.channels)
-            of.setframerate(f.samplerate)
-            of.setsampwidth(2)
-            for buf in f:
-                of.writeframes(buf)
-            print(of)
-elif(ext=='caf'):
-    with audioread.audio_open(filename) as f:
-#         print('Input file: %i channels at %i Hz; %.1f seconds.' %
-#               (f.channels, f.samplerate, f.duration),
-#               file=sys.stderr)
-#         print('Backend:', str(type(f).__module__).split('.')[1],
-#               file=sys.stderr)
-        filename_new = out + filename.split('/')[-1]
-        with contextlib.closing(wave.open(filename_new + '.wav', 'w')) as of:
-            of.setnchannels(f.channels)
-            of.setframerate(f.samplerate)
-            of.setsampwidth(2)
-            for buf in f:
-                of.writeframes(buf)
-elif(ext=='mp4'):
-    with audioread.audio_open(filename) as f:
-#         print('Input file: %i channels at %i Hz; %.1f seconds.' %
-#               (f.channels, f.samplerate, f.duration),
-#               file=sys.stderr)
-#         print('Backend:', str(type(f).__module__).split('.')[1],
-#               file=sys.stderr)
-        filename_new = out + filename.split('/')[-1]
-        with contextlib.closing(wave.open(filename_new + '.wav', 'w')) as of:
-            of.setnchannels(f.channels)
-            of.setframerate(f.samplerate)
-            of.setsampwidth(2)
-            for buf in f:
-                of.writeframes(buf)
-else:
-    filename_new = filename
-#-------------------------------end convert---------------------------------
+app = Flask(__name__)
+UPLOAD_FOLDER = 'C:/Users/91902/infantilevocaldecoder/tmp'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+ALLOWED_EXTENSIONS = {'wav'}
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
-#-----------------------------Feature Extraction----------------------------------------------------
-f=filename_new+'.wav'
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    
+    dir = 'C:/Users/91902/infantilevocaldecoder/tmp'
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
+        
+    dir2 = 'C:/Users/91902/infantilevocaldecoder/output'
+    for f in os.listdir(dir2):
+        os.remove(os.path.join(dir2, f))
+        
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'file uploaded successfully'
+
+if __name__ == '__main__':
+   app.run(debug = True)
